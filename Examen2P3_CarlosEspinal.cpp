1d2d0bea07afebf53cadd812a997e2f82a3d01e6
Carlos Espinal <cespinalb@gmail.com>
2023-09-22 13:35:10 -0600

    Cometi el primero

---
+++ Examen2P3_CarlosEspinal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void mainmenu() {
+    cout << "---- Main Menu ----\n" << "1. Leer archivos" << "2. Guardar archivos" << "3. Asignaciones" << "4. " 
+}
+
+int main(){
+    
+}
