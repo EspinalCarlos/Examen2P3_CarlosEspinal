5c17c1c23b4ef9355ae5c0730d94ee2542762522
Carlos Espinal <cespinalb@gmail.com>
2023-09-22 16:29:35 -0600

    S Z Orto

--- ScrumMaster.h
+++ ScrumMaster.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <string>
+#include "Proyecto.h"
+#include "Sprint.h"
+#include "Developer.h"
 #include <vector>
 #include <fstream>
 using namespace std;
 class ScrumMaster{
+	vector<Proyecto*> proyectos;
+	vector<Sprint*> sprints;
+public:
+	ScrumMaster(int id, string nCom, int aExp, string p);
+	~ScrumMaster();
 
 };
 
